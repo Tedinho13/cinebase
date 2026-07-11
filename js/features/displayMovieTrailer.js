36f6fabe597304d5de8a03f7294eb40667b330cc
Dariusz Skrzypczak <Tedinho08@gmail.com>
2026-07-11 11:29:39 +0200

    code cleaning

--- js/features/displayMovieTrailer.js
+++ js/features/displayMovieTrailer.js
@@ -5,12 +5,8 @@ import { renderMovieTrailer, showVideoModal } from "../ui/sharedUI.js";
 export const displayMovieTrailer = async (id) => {
   showVideoModal();
 
-  console.log(id);
   const url = getMovieVideoUrl(id);
   const data = await fetchData(url);
 
-  console.log(data);
-
   renderMovieTrailer(data.results);
 };
-
